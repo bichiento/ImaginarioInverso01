e0409e81c8b4f3135f63e4c9786302add26e0447
Microhom <microhom@gmail.com>
2014-10-23 17:15:03 -0500

    notas para funcionamiento

--- Arduino/LaserTest/LaserTest.ino
+++ Arduino/LaserTest/LaserTest.ino
@@ -1,7 +1,12 @@
 #define PIN_LED 13
+//conectar el arduino con transmisor al tx
+//conectar el arduino con receptor al rx
 
 byte msg[] = {
   'p', 'i', 'n', 'g', '\n'};
+  
+  // selecionar aleatoriamente de un archivo xml con el contenido del sitio un bloque 
+  // de texto a partir de marcadores 
 
 long long lastSendMillis;
 
@@ -18,6 +23,10 @@ void loop() {
     Serial.write(msg, 5);
     digitalWrite(PIN_LED, LOW);
   }
+
+// Imprimir el texto selecionado a la terminal 
+// experar x cantidad de segundos deacuerdo al lenght 
+//flush 
 
   if(Serial.available()){
     int rv = Serial.read();
