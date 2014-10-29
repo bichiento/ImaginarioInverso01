2e736670137e6f996dbfdb2d87b59ae6c4094d29
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-10-28 20:52:05 -0400

    arregla la puerta serial

--- Python/transmite.py
+++ Python/transmite.py
@@ -1,6 +1,6 @@
 import serial
 from time import sleep
-port = serial.Serial("/dev/ttyAMA0", 9600, 0.01)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.01, writeTimeout=0.5))
 while (True):
   port.write("HOLA ASTROS\n")
   sleep(1.0)
