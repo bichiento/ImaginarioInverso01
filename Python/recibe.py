5b303e52764bbd3533ce623efbffa7bc28b3886a
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-10-28 21:00:31 -0400

    solo imprimir data si hay data

--- Python/recibe.py
+++ Python/recibe.py
@@ -3,5 +3,6 @@ from time import sleep
 port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.01)
 while True:
   data = port.readline()
+  if data:
+    print data    
   sleep(0.1)
-  print data
